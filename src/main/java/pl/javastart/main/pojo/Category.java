1efcccf367437a0469246dab998b01a50cacf2f0
dominikkrypel <krypel.dominik@gmail.com>
2022-09-02 15:34:30 +0200

    add pojo and getters and setters, delete hard code body in project

---
+++ src/main/java/pl/javastart/main/pojo/Category.java
@@ -0,0 +1,38 @@
+package pl.javastart.main.pojo;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+
+@Generated("jsonschema2pojo")
+public class Category {
+
+    private Integer id;
+    private String name;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
