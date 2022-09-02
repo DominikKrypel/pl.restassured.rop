ed098304601209c7f8a00eef9d866e05f046f23a
dominikkrypel <krypel.dominik@gmail.com>
2022-09-02 15:02:55 +0200

    new tests and add to git ignore

--- src/test/java/pl/javastart/restassured/test/BasicHttpMethodsTests.java
+++ src/test/java/pl/javastart/restassured/test/BasicHttpMethodsTests.java
@@ -32,4 +32,116 @@ public class BasicHttpMethodsTests {
                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
                 .then().log().all().statusCode(200);
     }
+
+    @Test
+    public void createUser() {
+        String user = "{\"id\": 445,\n" +
+                "  \"username\": \"Alfa\",\n" +
+                "  \"firstName\": \"Dominik\",\n" +
+                "  \"lastName\": \"Dominikalski\",\n" +
+                "  \"email\": \"Dominik@test.com\",\n" +
+                "  \"password\": \"password\",\n" +
+                "  \"phone\": \"+123456789\",\n" +
+                "  \"userStatus\": 1}";
+
+        given().log().all().body(user).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
+                .then().log().all().statusCode(200);
+
+
+        given().log().all()
+                .pathParam("username", "Alfa")
+                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .then().log().all().statusCode(200);
+    }
+
+    @Test
+    public void givenExistingPetIdWhenGetPetThenReturnPetTest() {
+        given().log().method().log().uri()
+                .pathParam("petId", 1)
+                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
+                .then().log().all().statusCode(200);
+
+    }
+
+    @Test
+    public void givenExistingPetWhenUpdatePetNameThenPetIsChangedTest(){
+
+        String pet = "{\n" +
+                "  \"id\": 123,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"dogs\"\n" +
+                "  },\n" +
+                "  \"name\": \"KilerekAldi\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"http://photos.com/dog1.jpg\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 1,\n" +
+                "      \"name\": \"dogs-category\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+
+        pet = "{\n" +
+                "  \"id\": 123,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"dogs\"\n" +
+                "  },\n" +
+                "  \"name\": \"Anaconda\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"http://photos.com/dog1.jpg\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 1,\n" +
+                "      \"name\": \"dogs-category\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().put("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+        }
+
+        @Test
+    public void givenExistingPetIdWhenDeletingPetThenIsDeletedTest() {
+
+        String pet = "{\n" +
+                "  \"id\": 445,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"dogs\"\n" +
+                "  },\n" +
+                "  \"name\": \"dedimek\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"http://photos.com/dog1.jpg\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 1,\n" +
+                "      \"name\": \"dogs-category\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        given().log().all().body(pet).contentType("application/json")
+                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                 .then().log().all().statusCode(200);
+
+        given().log().all().contentType("application/json").pathParam("id", 445)
+                .when().delete("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{id}")
+                .then().log().all().statusCode(200);
+        }
 }
